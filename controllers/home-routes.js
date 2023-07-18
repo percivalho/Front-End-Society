2e3ae18090c531e48abcc46b9b4f66e2cf2e798c
percivalho <35653644+percivalho@users.noreply.github.com>
2023-07-18 19:04:15 +0100

    fix a number of bugs

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,14 +13,39 @@ router.get('/', async (req, res) => {
       //order: [['createdAt', 'DESC']],      
     });
 
-    const playlists = dbPlaylistData.map((playlist) =>
+    const playlistsAll = dbPlaylistData.map((playlist) =>
       playlist.get({ plain: true })
     );
+
+    // Function to shuffle the array
+    const shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+    }
+
+    // Shuffling the playlists array
+    shuffleArray(playlistsAll);
+
+    // Selecting the first 10 playlists after shuffle
+    const playlists = playlistsAll.slice(0, 10);
+
+    // Check if soundPlayed or not. If so, set it to true and send soundPlay as true to the frontend
+    /*let soundPlay = false;
+    if (req.session.soundPlayed === false) {
+      req.session.soundPlayed = true;
+      soundPlay = true;
+    } */
+    req.session.soundPlayed += 1;
+
+
     res.render('homepage', {
       playlists,
       loggedIn: req.session.loggedIn,
       username: req.session.username,
       sound: req.session.sound,
+      soundPlay: req.session.soundPlayed,
     });
   } catch (err) {
     console.log(err);
@@ -32,7 +57,7 @@ router.get('/', async (req, res) => {
 router.get('/playlist/:id', withAuth, async (req, res) => {
   // If the user is logged in, allow them to view the public playlist
   try {
-    const dbPlaylistData = await Playlist.findByPk(req.params.id, {
+    /*const dbPlaylistData = await Playlist.findByPk(req.params.id, {
       include: [
         {
           model: Song,
@@ -40,11 +65,29 @@ router.get('/playlist/:id', withAuth, async (req, res) => {
           as: 'songs',
         },
       ],
-    });
+    });*/
+    const dbPlaylistData = await Playlist.findOne({
+      where: {
+        id: req.params.id,
+        public: 1
+      },
+      include: [
+        {
+          model: Song,
+          through: PlaylistSong,
+          as: 'songs',
+        },
+      ],
+    });    
+
     if (dbPlaylistData){
       const playlist = dbPlaylistData.get({ plain: true });
       console.log(playlist);
-      res.render('playlist', { playlist, loggedIn: req.session.loggedIn });
+      res.render('playlist', { 
+        playlist, 
+        loggedIn: req.session.loggedIn,
+        username: req.session.username,
+      });
     } else {
       res.status(404).json({ message: 'No Playlist found with that id!' });
       return;
@@ -89,7 +132,11 @@ router.get('/myPlaylist', withAuth, async (req, res) => {
     
         if (dbPlaylistData){
           const playlist = dbPlaylistData.get({ plain: true });
-          res.render('myPlaylist', { playlist, loggedIn: req.session.loggedIn });
+          res.render('myPlaylist', { 
+            playlist, 
+            loggedIn: req.session.loggedIn,
+            username: req.session.username,
+          });
         }
       } catch (err) {
         console.log(err);
@@ -157,23 +204,46 @@ router.post('/myPlaylist/addSong/:id', withAuth, async (req, res) => {
           user_id: dbUserData.id,
         },
       });      
-      
       try {
         const dbPlaylistSongData = await PlaylistSong.create({
           playlist_id: dbPlaylistData.id,
           song_id: id,
         });
-        req.session.save(() => {
-          req.session.loggedIn = true;
-          req.session.username = req.session.username;    
-          res.status(200).json(dbPlaylistData);
-          //res.render('myPlaylist', { playlists, loggedIn: req.session.loggedIn });
+        // Query for the song data
+        const dbSongData = await Song.findByPk(id);
 
-        });
+        // If the song exists in the database, include the name in the response
+        if (dbSongData) {
+          req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.username = req.session.username; 
+
+            // Send back the song name as part of the response
+            res.status(200).json({
+              playlist: dbPlaylistData,
+              song: dbSongData.get({ plain: true })
+            });
+          });
+        }
       } catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
+        /*req.session.save(() => {
+          req.session.loggedIn = true;
+          req.session.username = req.session.username;    
+          res.status(200).json({
+            //dbPlaylistData);
+            playlist: dbPlaylistData,
+            song: dbSongData.get({ plain: true })
+          });            */
+          //res.render('myPlaylist', { playlists, loggedIn: req.session.loggedIn });
+
+        //});
+      /*} catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }*/
     }
   } catch (err) {
     console.log(err);
